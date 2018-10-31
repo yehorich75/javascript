47fca1247b00a9507e80931d487722536088a5fa
Yegorich75 <yegorich75@gmail.com>
2018-10-31 06:23:15 +0200

    for loop

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function myFunk() {
     var bgColor = document.querySelector("#header");
     bgColor.classList.toggle("header__open");
@@ -32,3 +34,56 @@ window.addEventListener('scroll', function() {
 //     }
 // }
 // window.addEventListener('scroll', redColor);
+
+
+const welcome = person => {
+    console.log(`Ave, ${person.name}`);
+};
+
+const persons = [
+    {name: 'Marcus Aurelius'},
+    {name: 'Mao Zedong'},
+    {name: 'Reme Descartes'}
+];
+
+for (const person of persons) welcome(person);
+
+// const welcome = person => {
+//     console.log(`Ave, ${person.name}`);
+// };
+
+// const marcus = { name: 'Marcus Aurelius'};
+// const mao = { name: 'Mao Zedong'};
+// const cartesius = {name: 'Reme Descartes'};
+
+// welcome(marcus);
+// welcome(mao);
+// welcome(cartesius);
+
+
+// const INTERVAL = 500;
+// let counter = 0;
+// const MAX_VALUE = 10;
+// let timer = null;
+
+// const event = () => {
+//     if (counter === MAX_VALUE) {
+//         console.log('The end');
+//         clearInterval(timer);
+//         return;
+//     }
+//     console.dir({ counter, date: new Date() });
+//     counter++;
+// };
+// console.log('Begin');
+// timer = setInterval(event, INTERVAL);
+
+
+// const MAX_VALUE = 10;
+
+// console.log('Begin');
+// for (let i = 0; i < MAX_VALUE; i++) {
+//     console.dir({i, date: new Date() });
+// }
+// console.log('The end');
+
